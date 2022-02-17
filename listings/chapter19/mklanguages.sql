1582d6317e11cc958a14e157440dccf94d44583b
Roman Spengler <github@r-spengler.de>
2022-02-17 11:30:36 +0100

    Typo entfernt

--- listings/chapter19/mklanguages.sql
+++ listings/chapter19/mklanguages.sql
@@ -15,7 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATADBASE IF NOT EXISTS programming_languages;
+CREATE DATABASE IF NOT EXISTS programming_languages;
 
 USE programming_languages;
 
